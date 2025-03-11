2163f097a8511a2586d15ecdd9d466515e1f53f3
Murillo Prevelato <prevelatomurillo@gmail.com>
2025-03-10 22:38:18 -0300

    .

---
+++ atv_nota/ex2.py
@@ -0,0 +1,7 @@
+altura = float(input("Digite sua altura em centímetros: "))
+
+if altura >= 150:
+    print("Você tem altura suficiente para entrar nesse brinquedo.")
+
+else: 
+    print("Desculpe, você não tem altura suficiente para entrar no brinquedo.")
