e0075010bc4793e1f8ba602d24f3f0d362bac773
murilloJAVA <prevelatomurillo@gmail.com>
2025-04-14 19:33:52 -0300

    .

--- Aula 8/exercicio7.py
+++ Aula 8/exercicio7.py
@@ -13,7 +13,4 @@ temperaturas = [-10, -8, 0, 1, 2, 5, -2, -4]
 
 print(f"A menor temperatura é {min(temperaturas)}")
 print(f"A maior temperatura é {max(temperaturas)}")
-print(f"A média das temperaturas é {sum(temperaturas) / len(temperaturas)}")
-
-print("as")
-              
+print(f"A média das temperaturas é {sum(temperaturas) / len(temperaturas)}")  
